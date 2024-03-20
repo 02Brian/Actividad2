351180cd273543f41e193269c0f8c361d740cb53
02Brian <163490664+02Brian@users.noreply.github.com>
2024-03-19 20:50:17 -0600

    Add files via upload
    Programa que al ingresar 2 números, realiza las operaciones que están destinadas.

---
+++ Suma, Resta, Multi, Division.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main () {
+    
+    int num1, num2, suma, resta, multiplicacion;
+    float division;
+    
+    cout<<"ingrese el 1er numero: ";
+    cin>>num1;
+    
+    cout<<"ingrese el 2do numero: ";
+    cin>>num2;
+    
+    
+suma= num1+num2;
+
+cout<<"la suma es:  "<<suma;
+
+resta= num1-num2;
+
+cout<<"\n la resta es:  "<<resta;
+
+multiplicacion= num1*num2;
+
+cout<<"\n la multiplicacion es:  "<<multiplicacion;
+
+
+division= num1/num2;
+cout<<"\n la division es:  "<<division;
+
+
+
+}
